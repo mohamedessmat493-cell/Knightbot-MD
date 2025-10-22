202fc1992f93b1eeb511588f0a5eedc323f601bb
mohamedessmat493-cell <mohamedessmat493@gmail.com>
2025-10-22 15:41:24 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'مًحًمًدٍ عٌصّـمًتٌـ ', // Your name
+  ownerNumber: '201095992005', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
